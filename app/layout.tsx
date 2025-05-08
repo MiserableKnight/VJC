aa3f033653f1e0d8a5b1c99ca6d345c2c1644b27
MiserableKnight <zhengqiao@live.com>
2025-05-08 10:28:17 +0700

    将移动端视图缩放设置为50%以优化移动设备显示效果

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,9 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   width: 'device-width',
-  initialScale: 1,
+  initialScale: 0.5,
   maximumScale: 5,
-  minimumScale: 1,
+  minimumScale: 0.5,
   userScalable: true,
   viewportFit: 'cover',
 };
